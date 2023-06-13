f8fc0022ec104add428af2e083e8402777ec0108
Guillaume Falourd <guillaume.falourd@zup.com.br>
2023-06-13 13:18:50 -0300

    update main.sh file sed command
    Signed-off-by: Guillaume Falourd <guillaume.falourd@zup.com.br>

--- main.sh
+++ main.sh
@@ -22,7 +22,7 @@ main() {
   do
     echo "KEY $i:${KEYS[$i]}"
     echo "VALUE $i:${VALUES[$i]}"
-    sed -i.back "s|${KEYS[$i]}|${VALUES[$i]|g" $FILE_PATH
+    sed -i.back 's|${KEYS[$i]}|${VALUES[$i]|g' $FILE_PATH
   done
 }
 
